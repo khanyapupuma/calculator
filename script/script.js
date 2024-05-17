ab089800d9cd633654f11e4d9c9cd2af5c882710
khanyapupuma <khalilpupuma@gmail.com>
2024-05-17 14:17:05 +0200

    calculator

--- script/script.js
+++ script/script.js
@@ -1,2 +1,14 @@
 let dataEntry = document.querySelector('[data-entry]')
-function display(value) { dataEntry.value += value}
+function display(value) { dataEntry.value += value};
+
+document.querySelector('[equals]').addEventListener('click', ()=>{
+    let entry = dataEntry.value
+    dataEntry.value = eval(entry)
+});
+
+let clearBtn = document.querySelector('[clear]')
+let screen = document.querySelector('#screen')
+
+clearBtn.addEventListener('click', () => {
+    screen.value = ''
+})
